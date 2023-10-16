28e67aa86c61740248138f1c9b7dd508379174fe
mayantha2004 <148047808+mayantha2004@users.noreply.github.com>
2023-10-16 13:56:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94715630586', 'me', true],
+  ['94715630586', 'maya', true], 
   [''] 
 ] //Number of owners
 
